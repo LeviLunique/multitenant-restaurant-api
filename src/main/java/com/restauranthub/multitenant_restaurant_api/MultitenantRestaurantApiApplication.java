bb591551b399d3f6cc9ed7b4295050c181dfcda4
levi.silva <levi.silva@agu.gov.br>
2026-03-19 22:56:11 -0300

    feat: inicializar estrutura do projeto Spring Boot

---
+++ src/main/java/com/restauranthub/multitenant_restaurant_api/MultitenantRestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.restauranthub.multitenant_restaurant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantRestaurantApiApplication.class, args);
+	}
+
+}
