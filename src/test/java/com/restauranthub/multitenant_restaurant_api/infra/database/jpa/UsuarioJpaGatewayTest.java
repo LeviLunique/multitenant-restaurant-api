65a404c4e49c73fd273013d113a5d9083ff69c58
levi.silva <levi.silva@agu.gov.br>
2026-03-22 20:05:41 -0300

    Corrige ocorrências de manutenção apontadas pelo Sonar

--- src/test/java/com/restauranthub/multitenant_restaurant_api/infra/database/jpa/UsuarioJpaGatewayTest.java
+++ src/test/java/com/restauranthub/multitenant_restaurant_api/infra/database/jpa/UsuarioJpaGatewayTest.java
@@ -3,8 +3,6 @@ package com.restauranthub.multitenant_restaurant_api.infra.database.jpa;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
